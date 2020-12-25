3e473d24a72e477c9ad643f655ae97d7963ae97f
Emil Burzo <contact@emilburzo.com>
2020-12-25 20:25:28 +0200

    update models

--- src/main/kotlin/com/emilburzo/db/models.kt
+++ src/main/kotlin/com/emilburzo/db/models.kt
@@ -5,19 +5,24 @@ import org.jetbrains.exposed.sql.jodatime.datetime
 
 
 object DbFlight : Table(name = "flights") {
+    // columns
     val id = long(name = "id")
     val start = datetime(name = "start").index() // https://github.com/JetBrains/Exposed/issues/221
     val pilotName = varchar(name = "pilot_name", length = 512)
     val pilotUsername = varchar(name = "pilot_username", length = 512)
     val takeoffName = varchar(name = "takeoff_name", length = 100).nullable()
-    val takeoffPoint = point(name = "takeoff_point").index()
+    val takeoffPoint = point(name = "takeoff_point")
     val type = varchar(name = "type", length = 512)
     val distanceKm = double(name = "distance_km")
     val score = double(name = "score")
     val airtime = integer(name = "airtime") // minutes
-    val glider_name = varchar(name = "glider_name", length = 50)
-    val glider_class = varchar(name = "glider_class", length = 50)
+    val gliderName = varchar(name = "glider_name", length = 50)
+    val gliderClass = varchar(name = "glider_class", length = 50)
     val url = varchar(name = "url", length = 512).uniqueIndex()
 
+    // custom indexes
+    val takeOffPointIndex = index(columns = arrayOf(takeoffPoint), indexType = "GIST")
+
+    // pk
     override val primaryKey = PrimaryKey(id)
 }
--- src/main/kotlin/com/emilburzo/service/models.kt
+++ src/main/kotlin/com/emilburzo/service/models.kt
@@ -1,13 +1,20 @@
 package com.emilburzo.service
 
+import org.postgis.Point
 import java.util.*
 
 data class Flight(
-    val title: String,
-    val distanceKm: Double,
-    val type: String,
+    val id: Long,
+    val start: Date,
     val pilotName: String,
     val pilotUsername: String,
+    val takeoffName: String?,
+    val takeoffPoint: Point,
+    val type: String,
+    val distanceKm: Double,
+    val score: Double,
+    val airtime: Int,
+    val gliderName: String,
+    val gliderClass: String,
     val url: String,
-    val flightDate: Date
 )
