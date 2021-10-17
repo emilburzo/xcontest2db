a00b43fd06d220b6f66688ed2924ae714dd8d8a7
Emil Burzo <contact@emilburzo.com>
2021-10-17 09:09:39 +0300

    navigation timeout should be in `gotoOptions`

--- src/main/kotlin/com/emilburzo/service/http/models.kt
+++ src/main/kotlin/com/emilburzo/service/http/models.kt
@@ -3,5 +3,9 @@ package com.emilburzo.service.http
 data class BrowserlessContent(
     val url: String,
     val waitFor: Int = 5_000,
+    val gotoOptions: BrowserlessGotoOptions
+)
+
+data class BrowserlessGotoOptions(
     val timeout: Int = 60_000
 )
