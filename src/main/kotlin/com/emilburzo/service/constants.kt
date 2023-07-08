4cb46873f95a35160c9196f4bd61845170628f58
Emil Burzo <contact@emilburzo.com>
2023-07-08 21:09:39 +0300

    update recent flights url

--- src/main/kotlin/com/emilburzo/service/constants.kt
+++ src/main/kotlin/com/emilburzo/service/constants.kt
@@ -1,3 +1,3 @@
 package com.emilburzo.service
 
-const val FLIGHTS_RECENT_URL = "https://www.xcontest.org/romania/zboruri/#flights[sort]=time_start" // todo env
+const val FLIGHTS_RECENT_URL = "https://www.xcontest.org/romania/zboruri/" // todo env
