c8b17660a814831394de10480ba28340656bccdc
Emil Burzo <contact@emilburzo.com>
2026-02-09 08:17:04 +0200

    support BROWSERLESS_URL env

--- src/main/kotlin/com/emilburzo/service/http/constants.kt
+++ src/main/kotlin/com/emilburzo/service/http/constants.kt
@@ -1,3 +1,3 @@
 package com.emilburzo.service.http
 
-const val BROWSERLESS_URL = "http://192.168.0.4:7094/content" // todo env
+val BROWSERLESS_URL: String = System.getenv("BROWSERLESS_URL") ?: "http://192.168.0.4:7094/content"
