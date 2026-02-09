4d8f360c57d2d91d1158253f72563b706816c35b
Emil Burzo <contact@emilburzo.com>
2026-02-09 08:10:39 +0200

    wait for a selector

--- src/main/kotlin/com/emilburzo/service/http/models.kt
+++ src/main/kotlin/com/emilburzo/service/http/models.kt
@@ -2,10 +2,11 @@ package com.emilburzo.service.http
 
 data class BrowserlessContent(
     val url: String,
-    val waitFor: Int = 5_000,
+    val waitFor: String = ".XClist",
     val gotoOptions: BrowserlessGotoOptions = BrowserlessGotoOptions()
 )
 
 data class BrowserlessGotoOptions(
     val timeout: Int = 60_000
 )
+
